f4eb968c1b8bab2e4d928631a4bdbf316b824fd3
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-09-17 14:27:02 -0700

    Created draft of index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,11 @@
 class TasksController < ApplicationController
+  TASKS = [
+    {name: "Task One", description: "Wash dishes", completion_date: "9/18/2018"},
+    {name: "Task Two", description: "Walk dog", completion_date: "9/20/2018"},
+    {name: "Task Three", description: "Do homework", completion_date: "9/23/2018"},
+
+  ]
   def index
+    @tasks = TASKS
   end
 end
