26012f2e0ad509ac9043ab2816afd73749c56641
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-09-17 16:46:13 -0700

    Created show method for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,19 @@
 class TasksController < ApplicationController
   TASKS = [
-    {name: "Task One", description: "Wash dishes", completion_date: "9/18/2018"},
-    {name: "Task Two", description: "Walk dog", completion_date: "9/20/2018"},
-    {name: "Task Three", description: "Do homework", completion_date: "9/23/2018"},
+    {id: 1, name: "Task One", description: "Wash dishes", completion_date: "9/18/2018"},
+    {id: 2, name: "Task Two", description: "Walk dog", completion_date: "9/20/2018"},
+    {id: 3, name: "Task Three", description: "Do homework", completion_date: "9/23/2018"},
 
   ]
   def index
     @tasks = TASKS
+  end
+
+  def show
+    task_id = params[:id].to_i
+    @task = TASKS[task_id]
+    if @task.nil?
+      head :not_found
+    end
   end
 end
