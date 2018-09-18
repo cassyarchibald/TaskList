f4d081f4e7894c01c848bffc64f3d03204d12849
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-09-18 14:46:46 -0700

    Completed adding two tasks/updating the Show method to use the databse

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,12 +6,12 @@ class TasksController < ApplicationController
 
   ]
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 
   def show
     task_id = params[:id].to_i
-    @task = TASKS[task_id]
+    @task = Task.find_by(id: task_id)
     if @task.nil?
       head :not_found
     end
