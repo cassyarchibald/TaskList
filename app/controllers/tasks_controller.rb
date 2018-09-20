2bd3ad83b72a7b3b65d8551c99132f62a5abfe6c
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-09-19 17:00:37 -0700

    Began work on edit and new files. Completed show file.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,53 @@
 class TasksController < ApplicationController
-  TASKS = [
-    {id: 1, name: "Task One", description: "Wash dishes", completion_date: "9/18/2018"},
-    {id: 2, name: "Task Two", description: "Walk dog", completion_date: "9/20/2018"},
-    {id: 3, name: "Task Three", description: "Do homework", completion_date: "9/23/2018"},
 
-  ]
   def index
     @tasks = Task.all
   end
 
   def show
+    # Show details for one book
     task_id = params[:id].to_i
     @task = Task.find_by(id: task_id)
     if @task.nil?
       head :not_found
+    end
+  end
+
+  def new
+    # Creates a new task available for new view to use
+      @task = Task.new
+  end
+
+  def create
+    # Uses post to send form data to the server/save a new book
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description], completion_date: params[:task][:completion_date])
+    if @task.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    # Form to edit details for an existing book. When you submit the form, it sends a patch request to update the databse
+    @task = Task.find(params[:id].to_i)
+    if @task.save
+      redirect_to task_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    # Sends form data to the server to update an existing book.
+    @book = Book.find(params[:id].to_i)
+  end
+
+  def delete
+    # Destroys an existing book and returns to home page
+    @task = Task.find(params[:id].to_i)
+    if @task.destroy
+      redirect_to root_path
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root "tasks#index"
   get '/tasks', to: 'tasks#index', as: 'tasks'
   get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  post '/tasks', to: 'tasks#create', as: 'book'
+  post '/tasks', to: 'tasks#create', as: 'create_tasks'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
   patch '/tasks/:id', to: 'tasks#update'
-  delete 'tasks/:id', to: 'tasks#destroy'
+  delete 'tasks/:id', to: 'tasks#destroy', as: 'destroy_task'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
